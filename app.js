503fc3419c4e8ceb69d9f1a36fd41cc86cddaf8a
Sz-Wei Chen <swchen1217@gmail.com>
2021-05-22 13:36:45 +0800

    app.js

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 'use strict'
 
-const path = require('path')
-const AutoLoad = require('fastify-autoload')
+require('dotenv').config();
+const path = require('path');
+const AutoLoad = require('fastify-autoload');
+const SequelizeFastify = require('sequelize-fastify');
+const Auth = require('./services/Auth');
+
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
 
-  // Do not touch the following lines
+  fastify.register(
+    SequelizeFastify,
+    {
+      instance: 'sequelize',
+      sequelizeOptions: {
+        dialect: 'mysql',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+      }
+    }
+  ).ready(async () => {
+    try {
+      await fastify.sequelize.authenticate();
+      fastify.log.info('Connection has been established successfully.');
+      // await fastify.sequelize.sync();
+    } catch (error) {
+      fastify.log.error('Unable to connect to the database:', error);
+    }
+  });
 
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
+  fastify.decorateRequest('user', null);
+  fastify.addHook('onRequest', Auth.verifyJwt(fastify));
+
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
-  })
+  });
 
-  // This loads all plugins defined in routes
-  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'models'),
+    options: Object.assign({}, opts)
+  });
+
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
     options: Object.assign({}, opts)
-  })
+  });
 }
